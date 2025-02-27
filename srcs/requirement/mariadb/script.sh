bafda2d8b3b98c44d29f77e5aa69e8bc25ef4afd
root <root@nadjemia.myguest.virtualbox.org>
2025-02-27 17:24:39 +0100

    nginx marche pas, je commence mariadb

---
+++ srcs/requirement/mariadb/script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+service mysql start;
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@% IDENTIFIED BY ;"
