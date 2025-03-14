40b0bff8bcbcf0008fc0f22538a1c6a0fe671577
root <root@nadjemia.myguest.virtualbox.org>
2025-03-14 16:17:50 +0100

    ca marche pas

--- srcs/requirement/mariadb/tools/mariadb_start.sh
+++ srcs/requirement/mariadb/tools/mariadb_start.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+
 service mysql start;
+
+until mysqladmin ping --silent; do
+	echo "En attente que MySQL demarre..."
+	sleep 2
+done
+
+echo "MySQL est demarre"
+
+mysql_install_db --user=mysql --ldata=/var/lib/mysql
+
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}'";
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost;' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -e "UPDATE mysql.user SET plugin='mysql_native_password' WHERE User='root';"
 mysql -e "FLUSH PRIVILEGES;"
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 exec mysqld_safe
