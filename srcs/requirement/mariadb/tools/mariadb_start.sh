e14b31ab0de4d8a8ee76fb7ce59ab9ba2a24c66c
nojia1775 <nojia.adjemrose@gmail.com>
2025-03-15 22:52:05 +0100

    haha super facile et tout ^^

--- srcs/requirement/mariadb/tools/mariadb_start.sh
+++ srcs/requirement/mariadb/tools/mariadb_start.sh
@@ -9,13 +9,15 @@ done
 
 echo "MySQL est demarre"
 
-mysql_install_db --user=mysql --ldata=/var/lib/mysql
+#mysql_install_db --user=mysql --ldata=/var/lib/mysql
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}'";
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mysql -e "UPDATE mysql.user SET plugin='mysql_native_password' WHERE User='root';"
-mysql -e "FLUSH PRIVILEGES;"
+mysql -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;"
+mysql -e "UPDATE mysql.user SET Host='%' WHERE User='root'"
+mysql -e "CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}'";
+mysql -u root -e "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;"
+mysql -u root -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}'; FLUSH PRIVILEGES;"
+#mysql -e "UPDATE mysql.user SET plugin='mysql_native_password' WHERE User='root';"
+#mysql -u root -p$SQL_ROOT_PASSWORD -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+#mysql -u root -p$SQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
 mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 exec mysqld_safe
