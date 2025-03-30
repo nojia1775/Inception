8088f4dc6a13b2129f4a52b6d550f4dddaf06bc2
nojia1775 <nojia.adjemrose@gmail.com>
2025-03-30 12:54:42 +0200

    probleme volumes nginx regle

--- srcs/requirement/nginx/tools/nginx.sh
+++ srcs/requirement/nginx/tools/nginx.sh
@@ -4,4 +4,10 @@ if [ -d /var/www/html ]; then
 	chmod -R 755 /var/www/html
 	find /var/www/html -type f -exec chmod 644 {} \;
 fi
+
+while [ ! -f /var/www/html/index.php ]; do
+	echo "waiting for wordpress to finish"
+	sleep 2
+done
+
 exec nginx -g "daemon off;"
